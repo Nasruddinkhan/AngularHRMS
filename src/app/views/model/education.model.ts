9d5a8fcef29d89fec1eb14311f044f362f5f8ece
Nasruddinkhan <nasruddinkhan44@gmail.com>
2019-12-30 01:45:49 +0530

    add education model

---
+++ src/app/views/model/education.model.ts
@@ -0,0 +1,12 @@
+export class EducationModel{
+    educationID:number;
+    fromDate:string;
+    toDate:string;
+    comments:string;
+    createdDate:Date; 
+    modifiedDate:Date;
+    modifiedBy:String;
+    activeStatus:number;
+    createdBy:String;
+    collegeName:string;
+}
--- src/app/views/user-information/eduction-details/eduction-details.component.ts
+++ src/app/views/user-information/eduction-details/eduction-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CourseService } from '../../service/course.service';
+import { UniversityService } from '../../service/university.service';
+import { NgForm } from '@angular/forms';
 /**
  * Created By, Nasruddin Khan
  * Created Date Aug 17, 2019 
@@ -12,19 +14,44 @@ import { CourseService } from '../../service/course.service';
 export class EductionDetailsComponent implements OnInit {
   eductionList = [];
   courseID: string;
+  univercityID:string;
   comments: string;
   collegeName: string
   univercity: string;
-
+  univercityList=[];
   courseList = [];
-  constructor(private courseService: CourseService) { }
+  startyear=new Date().getFullYear();
+  yearList=[];
+  toYear:string;
+  toMonth:string;
+  fromYear:string;
+  fromMonth:string;
+  constructor(private courseService: CourseService,private universityService:UniversityService) { }
 
   ngOnInit() {
+    this.getYears();
     this.getCourse();
-  }
+    this.getUniversity();
+    }
+    getYears(){
+      for(let i=this.startyear;i>this.startyear-25;i--){
+        this.yearList.push(i);
+      }
+    }
+    getUniversity(){
+      this.universityService.findAllUniversity().subscribe((res:any)=>{
+        this.univercityList=res;
+      });
+    }
   getCourse() {
     this.courseService.findAllCourse().subscribe((res: any) => {
       this.courseList = res;
-    })
+    });
+  }
+  onSubmit(form:NgForm){
+  //alert(JSON.stringify(form.value));
+  if(form.valid){
+
+  }
   }
 }
